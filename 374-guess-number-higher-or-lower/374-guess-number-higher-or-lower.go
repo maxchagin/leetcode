6f17cb12f97bbdba50e653b7df3d57e054a77feb
Maksim Chagin <maxchagin@gmail.com>
2024-12-09 23:49:56 +0300

    chore: 374-guess-number-higher-or-lower

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.go
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.go
@@ -2,8 +2,18 @@ package guessnumberhigherorlower
 
 var pick int
 
+func guess(num int) int {
+	if num > pick {
+		return -1
+	} else if num < pick {
+		return 1
+	} else {
+		return 0
+	}
+}
+
 // https://leetcode.com/problems/guess-number-higher-or-lower/
 func guessNumber(n int) int {
 
-	return 0
+	return -1
 }
